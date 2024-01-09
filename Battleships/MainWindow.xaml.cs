355445ca7b9ac1392568e443c58420a766187312
Blaadam <Adam.Worden06@gmail.com>
2024-01-09 14:24:29 +0000

    Ship Placement Auth

--- Battleships/MainWindow.xaml.cs
+++ Battleships/MainWindow.xaml.cs
@@ -20,43 +20,101 @@ namespace Battleships
     /// </summary>
     public partial class MainWindow : Window
     {
-        public string[,] BattleshipLocations = { { "", "", "" }, { "", "", "" }, { "", "","" } };
-        string[] NumberToLetterMap = { "A" , "B", "C", "D", "E", "F", "G"};
+        public const int NumberOfShips = 3;
+        public const int NumberOfPartsToAShip = 2;  // Dont change
+
+        public string[,] PlayerBattleshipLocations = new string[NumberOfShips, NumberOfPartsToAShip];
+        public string[,] OpponentBattleshipLocations = new string[NumberOfShips, NumberOfPartsToAShip];
+        string[] NumberToLetterMap = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z" };
+
+        public const int NumberOfRows = 6;
+        public const int NumberOfColumns = 6;
 
         public void SetShips(Grid ShipGrid)
         {
-
-            int count = 1;
-
-            for (int i = 0; i < 6; i++)
+            // Collumns
+            for (int i = 0; i < NumberOfColumns; i++)
             {
-                for (int j = 0; j < 6; j++)
+                // Rows
+                for (int j = 0; j < NumberOfRows; j++)
                 {
                     Button MyControl1 = new Button();
-                    MyControl1.Content = count.ToString();
-                    MyControl1.Name = NumberToLetterMap[i] + j.ToString();
+                    MyControl1.Content = NumberToLetterMap.GetValue(i) + j.ToString();
+                    MyControl1.Name = NumberToLetterMap.GetValue(i) + j.ToString();
                     MyControl1.Click += Button_Click;
 
                     Grid.SetColumn(MyControl1, j);
                     Grid.SetRow(MyControl1, i);
-                    PlayerShips.Children.Add(MyControl1);
-                    count = count + 1;
+                    ShipGrid.Children.Add(MyControl1);
                 }
             }
         }
+
+        public bool AuthorisedShipPlacement(string ShipPlacement)
+        {
+            int ShipSlot = -1;
+            int ShipButtonSlot = -1;
+
+            // Get total number of ships to place
+            //for (int i = 0; i < PlayerBattleshipLocations.Length-1; i++)
+            for (int i = 0; i < NumberOfShips - 1; i++)
+            {
+                // Get number of spaces per ship
+                for (int i_ = 0; i_ < NumberOfPartsToAShip - 1; i_++)
+                {
+                    // Check if its a new ship
+                    if (i_ == 0 && PlayerBattleshipLocations[i, i_] == null)
+                    {
+                        ShipButtonSlot = 0;
+                        ShipSlot = i;
+                        break;
+                    }
+                    // Checks if its contributing to a current ship slot
+                    else if (PlayerBattleshipLocations[i, i_] == null)
+                    {
+                        ShipButtonSlot = i_;
+                        ShipSlot = i;
+                    }
+                }
+
+                if (ShipSlot == -1 || ShipButtonSlot == -1)
+                {
+                    break;
+                }
+
+            }
+
+            if (ShipSlot == -1 || ShipButtonSlot == -1)
+            {
+                return false;
+            }
+
+            PlayerBattleshipLocations[ShipSlot, ShipButtonSlot] = ShipPlacement;
+            //Label1.Content = PlayerBattleshipLocations[ShipSlot, ShipButtonSlot];
+
+            return true;
+        }
+
+        public String CurrentTurn = "ShipSelect";
 
         public MainWindow()
         {
             InitializeComponent();
 
             SetShips(PlayerShips);
-            
-
+            SetShips(PlayerShots);
         }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Button b = (Button)sender;
-            b.Background = Brushes.Red;
+
+            if (CurrentTurn == "ShipSelect" && PlayerShips.IsAncestorOf(b))
+            {
+                if (AuthorisedShipPlacement(b.Name))
+                {
+                    b.Background = Brushes.Red;
+                }
+            }
         }
 
     }
